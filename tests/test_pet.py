cf576f75f472ebcca1e0ea967ce2343c96c22261
Виталий <sertelepat12@gmail.com>
2026-05-19 17:13:51 +0500

    Добавил тест Добавление питомца с полными данными

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -52,13 +52,12 @@ class TestPet:
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet not found", "Текст ошибки не совпал с ER"
 
-    @allure.title("Добавление нового питомца")
+    @allure.title("Добавление нового питомца с неполными данными")
     def test_add_new_pet(self):
-        pet_id = 1
         with allure.step("Отправление запроса на добавление нового питомца"):
             body_send_request = {
-                "id": pet_id,
-                "name": "Tuzik",
+                "id": 1,
+                "name": "Buddy",
                 "status": "available"
             }
 
@@ -74,4 +73,40 @@ class TestPet:
         with allure.step("Проверка на соответствие отправленных и полученных значений"):
             assert response_json["id"] == body_send_request["id"], "ID не совпал с ER"
             assert response_json["name"] == body_send_request["name"], "Имя питомца не совпало с ER"
+            assert response_json["status"] == body_send_request["status"], "Статус не воспал с ER"
+
+    @allure.title("Добавление нового питомца с полными данными")
+    def test_add_new_pet_with_full_datas(self):
+        with allure.step("Отправление запроса на добавление нового питомца"):
+            body_send_request = {
+                "id": 10,
+                "name": "doggie",
+                "category": {
+                    "id": 1,
+                    "name": "Dogs"
+                },
+                "photoUrls": ["string"],
+                "tags": [{
+                    "id": 0,
+                    "name":
+                        "string"
+                }],
+                "status": "available"
+            }
+
+            response = requests.post(url=f"{BASE_URL}/pet", json=body_send_request)
+
+        with allure.step("Проверка статус-кода ответа"):
+            assert response.status_code == 200, "Статус кода не совпал с ER"
+
+        with allure.step("Валидация JSON-схемы"):
+            response_json = response.json()
+            jsonschema.validate(response_json, PET_SCHEMA)
+
+        with allure.step("Проверка на соответствие отправленных и полученных значений"):
+            assert response_json["id"] == body_send_request["id"], "ID не совпал с ER"
+            assert response_json["name"] == body_send_request["name"], "Имя питомца не совпало с ER"
+            assert response_json["category"] == body_send_request["category"], "Категория не совпала с ER"
+            assert response_json["photoUrls"] == body_send_request["photoUrls"], "Ссылка на фото не совпала с ER"
+            assert response_json["tags"] == body_send_request["tags"], "Теги не совпали с ER"
             assert response_json["status"] == body_send_request["status"], "Статус не воспал с ER"
